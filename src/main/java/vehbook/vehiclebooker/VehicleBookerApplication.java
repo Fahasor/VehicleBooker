f1bd0f3bc93638cb48cb0226f77559f831de5604
Fahasor <rad.nikita@list.ru>
2024-03-15 12:44:30 +0300

    initial commit

---
+++ src/main/java/vehbook/vehiclebooker/VehicleBookerApplication.java
@@ -0,0 +1,13 @@
+package vehbook.vehiclebooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleBookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleBookerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vehbook/vehiclebooker/VehicleBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package vehbook.vehiclebooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleBookerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
