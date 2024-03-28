b09c4c257555d544b3a95a02da3cc9e9171afc41
Fahasor <rad.nikita@list.ru>
2024-03-28 21:02:29 +0300

    code cleanup

--- src/main/java/vehbook/vehiclebooker/model/User.java
+++ src/main/java/vehbook/vehiclebooker/model/User.java
@@ -1,25 +1,13 @@
 package vehbook.vehiclebooker.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class User {
     private Long id;
     private String phoneNumber;
-
-    public User(Long id, String phoneNumber) {
-        this.id = id;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
