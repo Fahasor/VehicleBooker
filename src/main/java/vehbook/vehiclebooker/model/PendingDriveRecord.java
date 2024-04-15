afaf72cc1f64db60e8dff47ab88599e7c498cd19
Fahasor <rad.nikita@list.ru>
2024-04-15 09:27:37 +0300

    fix PendingDriveRecord entity

--- src/main/java/vehbook/vehiclebooker/model/PendingDriveRecord.java
+++ src/main/java/vehbook/vehiclebooker/model/PendingDriveRecord.java
@@ -1,13 +1,13 @@
 package vehbook.vehiclebooker.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Table
 @Entity
 @Data
+@NoArgsConstructor
 public class PendingDriveRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
