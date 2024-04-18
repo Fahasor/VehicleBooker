bf5197b420583b07e56b3a32022b9166b9f3f802
Fahasor <rad.nikita@list.ru>
2024-04-18 09:49:53 +0300

    Add reference to another tables

--- src/main/java/vehbook/vehiclebooker/model/PendingDriveRecord.java
+++ src/main/java/vehbook/vehiclebooker/model/PendingDriveRecord.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -11,4 +13,10 @@ public class PendingDriveRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     long id;
+
+    @ManyToMany
+    List<UserIdentity> assignedUsers;
+
+    @ManyToOne
+    DriverIdentity driver;
 }
