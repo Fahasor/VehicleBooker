fff093c94dd78d0a5484b32a1cb304134dd9757b
Fahasor <rad.nikita@list.ru>
2024-05-01 23:29:17 +0300

    add class for cache

---
+++ src/main/java/vehbook/vehiclebooker/cache/Cache.java
@@ -0,0 +1,29 @@
+package vehbook.vehiclebooker.cache;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+@AllArgsConstructor
+public class Cache<K, V> {
+    private final HashMap<K, V> map;
+
+    public V get(K key) {
+        return map.get(key);
+    }
+    public void assign(K key, V value) {
+        if(map.containsKey(key))
+        {
+            map.replace(key, value);
+        }
+        else
+        {
+            map.put(key, value);
+        }
+    }
+    public void wipe(K key) {
+        map.remove(key);
+    }
+}
