04a5be7c6a1a377cd1fc4db7dfbe430678ee10ca
Fahasor <rad.nikita@list.ru>
2024-05-21 14:00:31 +0300

    rework DriverCache assign operation

--- src/main/java/vehbook/vehiclebooker/cache/DriverCache.java
+++ src/main/java/vehbook/vehiclebooker/cache/DriverCache.java
@@ -1,26 +1,24 @@
 package vehbook.vehiclebooker.cache;
 
+import java.util.TreeMap;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-
 @Component
-public class DriverCache<K, V> {
-    private final HashMap<K, V> map;
-    private final int maxCapacity;
+public class DriverCache<K extends Comparable<K>, V> {
+    private final TreeMap<K, V> map;
+    private final static int maxCapacity = 200;
 
     public DriverCache() {
-        map = new HashMap<>();
-        this.maxCapacity = 200;
+        map = new TreeMap<>();
     }
 
     public V get(K key) {
         return map.get(key);
     }
 
-    public boolean assign(K key, V value) {
+    public void assign(K key, V value) {
         if(map.size() >= maxCapacity) {
-            return false;
+            map.pollLastEntry();
         }
 
         if(map.containsKey(key)) {
@@ -29,8 +27,6 @@ public class DriverCache<K, V> {
         else {
             map.put(key, value);
         }
-
-        return true;
     }
 
     public void wipe(K key) {
