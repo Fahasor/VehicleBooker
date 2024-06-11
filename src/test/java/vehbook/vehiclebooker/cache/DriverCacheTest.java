40d9db6fa28931d834981add555d9f8973bac129
Fahasor <rad.nikita@list.ru>
2024-06-11 16:07:14 +0300

    add DriveRecordControllerTest

--- src/test/java/vehbook/vehiclebooker/cache/DriverCacheTest.java
+++ src/test/java/vehbook/vehiclebooker/cache/DriverCacheTest.java
@@ -2,41 +2,43 @@ package vehbook.vehiclebooker.cache;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import vehbook.vehiclebooker.DriverCache;
 
 class DriverCacheTest {
 
   @Test
   void testGetMaxCapacity() {
-    vehbook.vehiclebooker.DriverCache<Integer, String> driverCache = new vehbook.vehiclebooker.DriverCache<>();
+    DriverCache<Integer, String> driverCache = new DriverCache<>();
     assertEquals(200, driverCache.getMaxCapacity());
   }
 
   @Test
   void testGet() {
-    vehbook.vehiclebooker.DriverCache<Integer, String> driverCache = new vehbook.vehiclebooker.DriverCache<>();
+    DriverCache<Integer, String> driverCache = new DriverCache<>();
     driverCache.assign(1, "driver1");
     assertEquals("driver1", driverCache.get(1));
   }
 
   @Test
   void testGetNonExistingKey() {
-    vehbook.vehiclebooker.DriverCache<Integer, String> driverCache = new vehbook.vehiclebooker.DriverCache<>();
+    DriverCache<Integer, String> driverCache = new DriverCache<>();
     assertNull(driverCache.get(1));
   }
 
   @Test
   void testAssign() {
-    vehbook.vehiclebooker.DriverCache<Integer, String> driverCache = new vehbook.vehiclebooker.DriverCache<>();
+    DriverCache<Integer, String> driverCache = new DriverCache<>();
     driverCache.assign(1, "driver1");
     assertEquals("driver1", driverCache.get(1));
+    // Вместо прямого обращения к map, используем get()
+    assertTrue(driverCache.get(1) != null);
   }
 
   @Test
   void testAssignExistingKey() {
-    vehbook.vehiclebooker.DriverCache<Integer, String> driverCache = new vehbook.vehiclebooker.DriverCache<>();
+    DriverCache<Integer, String> driverCache = new DriverCache<>();
     driverCache.assign(1, "driver1");
     driverCache.assign(1, "driver2");
     assertEquals("driver2", driverCache.get(1));
@@ -44,18 +46,20 @@ class DriverCacheTest {
 
   @Test
   void testAssignOverCapacity() {
-    vehbook.vehiclebooker.DriverCache<Integer, String> driverCache = new vehbook.vehiclebooker.DriverCache<>();
+    DriverCache<Integer, String> driverCache = new DriverCache<>();
     for (int i = 0; i < 201; i++) {
       driverCache.assign(i, "driver" + i);
     }
+    assertEquals(200, driverCache.get(200) != null ? 200 : 199);
     assertNull(driverCache.get(0));
   }
 
   @Test
   void testWipe() {
-    vehbook.vehiclebooker.DriverCache<Integer, String> driverCache = new DriverCache<>();
+    DriverCache<Integer, String> driverCache = new DriverCache<>();
     driverCache.assign(1, "driver1");
     driverCache.wipe(1);
+    assertNull(driverCache.get(1));
     assertNull(driverCache.get(1));
   }
 }
