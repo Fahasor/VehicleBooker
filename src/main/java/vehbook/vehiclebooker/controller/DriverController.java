67f56f5d121292656de222b94fc11ea2aab3e117
Fahasor <rad.nikita@list.ru>
2024-08-18 21:17:19 +0300

    allow CORS

--- src/main/java/vehbook/vehiclebooker/controller/DriverController.java
+++ src/main/java/vehbook/vehiclebooker/controller/DriverController.java
@@ -2,6 +2,7 @@ package vehbook.vehiclebooker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/drivers")
+@CrossOrigin
 public class DriverController {
 
   private final DriverService driverService;
